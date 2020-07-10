73af5000942c4c16fcfeef11bc43976f1a442f75
OlegMatveevS <55399599+OlegMatveevS@users.noreply.github.com>
2020-07-10 16:17:58 +0800

    calcTimer

--- utils.php
+++ utils.php
@@ -1,1 +1,20 @@
 <?php
+$xValues = [-3, -2, -1, 0, 1, 2, 3, 4, 5];
+$rValues = [1, 2, 3, 4, 5];
+$yMax = 5;
+$yMin = -3;
+
+$areaImg = '<img id="areas-img" src="static/images/areas.PNG">';
+
+function calcTimer($start, $end) {
+    $startCalcTimer = explode(' ', $start);
+    $endCalcTimer = explode(' ', $end);
+
+    $calcTimerSec = $endCalcTimer[1] - $startCalcTimer[1];
+    $calcTimerMsec = $endCalcTimer[0] - $startCalcTimer[0];
+
+    if ($calcTimerSec === 0)
+        return round($calcTimerMsec, 10);
+    else
+        return $calcTimerSec + $calcTimerMsec;
+}
